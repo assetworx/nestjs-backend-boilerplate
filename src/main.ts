72d61649cbd8d8323258ae19bfb0c5c8892f8328
Christian Versloot <mail@christianversloot.nl>
2019-08-21 14:25:40 +0200

    Enable CSURF against csrf

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { constants } from './constants';
 import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   // Create the app
@@ -18,6 +19,8 @@ async function bootstrap() {
   app.use(helmet());
   // Enable Cross-origin resource sharing
   app.enableCors();
+  // Enable CSURF aganst cross-site request forgery
+  app.use(csurf());
   // Start listening on port number defined in constants
   await app.listen(constants.httpPort);
 }
