a4cebb1255e9a78727a98b3bf98809d22af2ee86
Christian Versloot <mail@christianversloot.nl>
2019-12-06 14:53:28 +0100

    Add note about applicability of LocalStrategy

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -3,6 +3,15 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
+/**
+ * ==================== IMPORTANT! ====================
+ * Passport-local works only with (username, password) combinations.
+ * See: http://www.passportjs.org/docs/username-password/
+ * Hence, if you wish to use a different 'local strategy' (e.g. email / magic link based login), you
+ * cannot use this Strategy.
+ * In that case, use `passport-custom` instead: https://www.npmjs.com/package/passport-custom
+ */
+
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
 
